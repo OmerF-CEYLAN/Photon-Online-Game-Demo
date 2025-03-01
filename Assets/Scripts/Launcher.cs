dbe18aaa2a21b003215481c5d52eb79146932e72
OmerF-CEYLAN <omerfceylan2004@gmail.com>
2025-03-01 15:40:24 +0300

    Player List added

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -31,6 +31,9 @@ public class Launcher : MonoBehaviourPunCallbacks
     [SerializeField]
     GameObject PlayerListItemPrefab;
 
+    [SerializeField]
+    GameObject startGameButton;
+
     void Awake()
     {
         Instance = this;
@@ -46,6 +49,7 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         Debug.Log("Connected to master");
         PhotonNetwork.JoinLobby();//Oda bulma ya da oda oluþturmayý lobide yaparýz.
+        PhotonNetwork.AutomaticallySyncScene = true; // Host sahne deðiþtirdiðinde (oyunu açtýðýnda) clientlar (diðer oyuncular) için de sahneyi deðiþtiriyor.
     }
 
     public override void OnJoinedLobby()
@@ -76,12 +80,25 @@ public class Launcher : MonoBehaviourPunCallbacks
         {
             Instantiate(PlayerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
         }
+
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient); //Odanýn kurucusuysa oyunu baþlatma butonu aktif deðilse aktif deðil.
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient) //Eðer master client ayrýlýrsa Master client rolü baþkasýna geçerken çalýþýr.
+    { //Metod tüm clientlar için çalýþacak ve hangi client master olduysa onun için Start game butonu aktifleþtirilecek. 
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient); 
     }
 
     public override void OnCreateRoomFailed(short returnCode,string message)
     {
         errorText.text = "Room Creation Failed " + message;
         MenuManager.Instance.OpenMenu("error");
+    }
+
+    public void StartGame()
+    {
+        PhotonNetwork.LoadLevel(1); //Build penceresinde game sahnesinin indexi 1 idi o yüzde onu parametre geçtik.
+        // Bu metod Hem host hem de diðer oyuncular için sahneyi baþlatýyor. Bundan dolayý Unity'nin kendi scene managementýný kullanmýyoruz.
     }
 
     public void LeaveRoom()
