a0ea359b875faf445899568ce50f2728d725f43f
OmerF-CEYLAN <omerfceylan2004@gmail.com>
2025-03-02 14:53:40 +0300

    Player Synchronize

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,29 @@
+using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class PlayerManager : MonoBehaviour
+{
+    PhotonView PV;
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    void Start()
+    {
+        if(PV.IsMine)
+        {
+            CreateController();
+        }
+    }
+
+    private void CreateController()
+    {
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), Vector3.zero, Quaternion.identity);
+    }
+}
